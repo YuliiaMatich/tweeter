136b4851b6f57e22c860dd24db9dc564bed71230
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-19 19:33:41 -0400

    styling of the tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {  //  runs a callback when the DOM is ready to be manipulated with jQuery
   $('#tweet-text').on('input', function () { // when element wit id "tweet-text" gets and input, call back function is called;
     $('.counter').text(140 - this.value.length); // "counter class is called. this.value = intput value of tweet text"
-    if ( $('.counter').text() < 0 ) {
+    if ( $('.counter').text() < 0 ) { // change color if counter < 0;
       $('.counter').addClass('counter-error');
     } else {
       $('.counter').removeClass('counter-error');
