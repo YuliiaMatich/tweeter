8ad0f8584617b2902391e0576efc2679b5e39244
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-19 16:35:19 -0400

    add counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {  //  runs a callback when the DOM is ready to be manipulated with jQuery
+  $('#tweet-text').on('input', function () { // when element wit id "tweet-text" gets and input, call back function is called;
+    $('.counter').text(140 - this.value.length); // "counter class is called. this.value = intput value of tweet text"
+    if ( $('.counter').text() < 0 ) {
+      $('.counter').addClass('counter-error');
+    }
+  })
+});
+
+
