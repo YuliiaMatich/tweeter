d7e97e9ff22d6fd64d94b7f902981499770f9f28
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-20 18:10:11 -0400

    add loadtweets function and timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () { // to add it to the html page when document is loaded to the browser 
-
-  const data = [
-    {
-      "user": {
-        "name": "Apollo Alien",
-        "avatars": "/images/avatar.png"
-        ,
-        "handle": "@MrApollo"
-      },
-      "content": {
-        "text": "First alien's tweet"
-      },
-      "created_at": 1461116232228
-    },
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = function (tweetData) {
     const markup = `
   <article class="tweet">
@@ -55,7 +16,7 @@ $(document).ready(function () { // to add it to the html page when document is l
         </header>
         <div class="added-tweet-text">${tweetData.content.text}</div>
         <footer>
-          <p>${tweetData.created_at}</p>
+          <p>${timeago.format(tweetData.created_at)}</p>
           <div class="icons">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -87,8 +48,14 @@ $(document).ready(function () { // to add it to the html page when document is l
     });
   });
 
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' }) // Ajax GET request to  /tweets to server
+    .then(function (serverTweets) { // callback when service responds
+      renderTweets(serverTweets); // send received array with tweets to renderTweets function
+    });
+  }
 
-  renderTweets(data)
+  loadTweets();
 });
 
 
