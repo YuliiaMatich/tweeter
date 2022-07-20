a48cdb6b49cadeeea8ccb68c30fcd35793c3037a
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-20 15:47:24 -0400

    add createTweetElement function and renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,24 +3,49 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () { // to add it to the html page when document is loaded to the browser 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Apollo Alien",
+        "avatars": "/images/avatar.png"
+        ,
+        "handle": "@MrApollo"
+      },
+      "content": {
+        "text": "First alien's tweet"
+      },
+      "created_at": 1461116232228
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "created_at": 1461116232227
-};
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-
-
-const createTweetElement = function(tweetData) {
-  const markup = `
-  <article class="tweets">
+  const createTweetElement = function (tweetData) {
+    const markup = `
+  <article class="tweet">
         <header>
           <div class="image-name">
           <img class ="user-image"src="${tweetData.user.avatars}"> 
@@ -29,7 +54,7 @@ const createTweetElement = function(tweetData) {
           <div class="user-name">${tweetData.user.handle}</div>
         </header>
         <div class="added-tweet-text">${tweetData.content.text}</div>
-        <footer class="tweet-footer">
+        <footer>
           <p>${tweetData.created_at}</p>
           <div class="icons">
           <i class="fa-solid fa-flag"></i>
@@ -39,11 +64,19 @@ const createTweetElement = function(tweetData) {
         </footer>
       </article>
   `
-  return markup;
-}
+    return markup;
+  }
 
-const $tweet = createTweetElement(tweetData);
+  const renderTweets = function (tweets) {
 
-$(document).ready(function() { // to ad it to the html page when document is loaded to the browser 
-  $('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    for (user of tweets) {// loops through tweets
+      const $tweet = createTweetElement(user); // calls createTweetElement for each tweet
+      $('#tweets-container').prepend($tweet);// takes return value and appends it to the tweets container
+    }
+  }
+
+
+
+  renderTweets(data)
 });
+
