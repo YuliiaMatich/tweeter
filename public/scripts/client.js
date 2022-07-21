a719cd6e4cb763f806fb7a99ccf71d0f6a26da14
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-21 18:44:53 -0400

    fix asyncronus bug

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,7 +29,7 @@ $(document).ready(function () { // to add it to the html page when document is l
   }
 
   const renderTweets = function (tweets) {
-    const container =  $('#tweets-container');
+    const container = $('#tweets-container');
     container.empty();
     for (user of tweets) {// loops through tweets
       const $tweet = createTweetElement(user); // calls createTweetElement for each tweet
@@ -40,17 +40,18 @@ $(document).ready(function () { // to add it to the html page when document is l
 
   const $form = $('.new-tweet-form');
   $form.on('submit', function (event) { // event listener
+    console.log('submit')
     event.preventDefault(); // to prevent the default form submission behaviour
     console.log($('#tweet-text').val());
     if (!$('#tweet-text').val()) {
       $('.error-message').slideUp();
       return $('.error-message').text('Your tweet is empty.').slideDown();
     }
-    
+
     if ($('#tweet-text').val().length > 140) {
       $('.error-message').slideUp();
       return $('.error-message').text('Characters limit was exceeded.').slideDown();
-      
+
     }
     $('.error-message').slideUp();
 
@@ -59,13 +60,15 @@ $(document).ready(function () { // to add it to the html page when document is l
       url: "/tweets",
       data: $('.new-tweet-form').serialize()
     })
-    .then(loadTweets());
+      .then(() => {
+        loadTweets()
+      });
   });
 
 
   const loadTweets = function () {
     $.ajax('/tweets', { method: 'GET' }) // Ajax GET request to  /tweets to server
-      .then(function (serverTweets) { // callback when service responds
+      .then(serverTweets => { // callback when service responds
         renderTweets(serverTweets); // send received array with tweets to renderTweets function
       });
   }
